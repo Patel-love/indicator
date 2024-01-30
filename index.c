76bf3d16831d2eedf80371993b094ff9783634f9
patel-love <manavamrutiya29@gmail.com>
2024-01-30 11:19:37 +0530

    pointer

---
+++ index.c
@@ -0,0 +1,64 @@
+// -------------------------Q-1----------------------
+
+// #include <stdio.h>
+
+
+// void fun(int a, int b){
+//     printf("Before swapping = a :-  %d\n", a) ;
+//     printf("Before swapping = b :-  %d\n\n", b) ;
+//     int temp;
+//     temp=a ;
+//     a=b;
+//     b=temp ;
+//     printf("After swapping = a :-   %d\n",a);
+//     printf("After swapping = b :-   %d\n",b);
+    
+// }
+// int main()
+// {
+//    int a=5;
+//    int b=3;
+//    fun(a,b);
+
+//     return 0;
+// }
+
+
+// -------------------------Q-2----------------------
+
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[5]={4,5,6,7,8};
+//     int *m;
+//     m=&arr;
+    
+//     printf("%u :- %d\n",m,*m);
+//     printf("%u :- %d\n",m+1,*(m+1));
+//     printf("%u :- %d\n",m+2,*(m+2));
+//     printf("%u :- %d\n",m+3,*(m+3)) ;
+//     printf("%u :- %d\n",m+4,*(m+4));
+    
+//     return 0;
+// }
+
+
+
+// -------------------------Q-3----------------------
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//    int a=20;
+//    int *p=&a;
+//    int **p2;
+//    p2=&p ;
+//    printf("%u :- %d",p2,**p2);
+   
+//     return 0;
+// }
